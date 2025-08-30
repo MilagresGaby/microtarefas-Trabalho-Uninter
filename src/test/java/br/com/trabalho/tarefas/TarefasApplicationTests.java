bfbba2a5419018bfb69323e3491f1750f806c54f
MilagresGaby <153197266+MilagresGaby@users.noreply.github.com>
2025-08-30 17:36:13 -0300

    Criação do projeto

---
+++ src/test/java/br/com/trabalho/tarefas/TarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
